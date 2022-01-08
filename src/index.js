6922db14bf47cc1a311c467ca960efccb50a6aad
epotapp <93080355+epotapp@users.noreply.github.com>
2022-01-08 22:53:17 +0300

    feat: write function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let arr = n.toString().split('').reverse()
+  if (arr.includes('-')) {
+      arr.pop(); return arr.join('')
+    } else return arr.join('') 
 }
+
